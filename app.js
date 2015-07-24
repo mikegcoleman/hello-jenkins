26a081aa1fad6212676e64a4eb45d85d8eb3109b
Mike Coleman <mikecoleman@Mikes-MBP.hsd1.or.comcast.net>
2015-07-24 09:37:32 -0700

    Add node app version 1

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+ 
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+ 
+app.listen(process.env.PORT || 5000);
+ 
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../app.js');
+ 
+describe('GET /', function() {
+  it('respond with hello world', function(done) {
+    request(app).get('/').expect('hello world', done);
+  });
+});
