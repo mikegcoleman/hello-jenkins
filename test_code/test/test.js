e408d7d00bf2b2d8688bb6368e64b2ec4769caa5
Mike Coleman <mikegcoleman@gmail.com>
2015-08-13 10:30:00 -0700

    cleaned up directory structure

--- test_code/test/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('supertest');
-var app = require('../app.js');
- 
-describe('GET /', function() {
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
-  });
-});
