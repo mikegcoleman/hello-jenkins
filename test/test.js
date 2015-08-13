9e70d706d96a7562941d67f558d01e6a703555fa
Matt Bentley <mbentley@mbentley.net>
2015-08-13 12:01:27 -0400

    Fix test description
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../app.js');
- 
+
 describe('GET /', function() {
-  it('respond with hello world', function(done) {
+  it('respond with hello jenkins', function(done) {
     request(app).get('/').expect('hello jenkins', done);
   });
 });
